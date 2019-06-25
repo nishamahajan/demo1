7c041f5528fb5bfc1b84435e5c9a6f913fd85f6b
user40 <user40@dypm.org>
2019-06-25 13:53:09 +0530

    hello1

--- server1.js
+++ server1.js
@@ -5,9 +5,9 @@ var app = express();
 var ordersController=function(req, res){
   console.log("Invoking  rest api for product list");
   var orders = [
-    {id:1, customer:"Raj",amount:500, status:"completed"},
-    {id:2, customer:"Simran",amount:1500, status:"inprogress"},
-    {id:3, customer:"Ram",amount:2500, status:"rejected"}
+    {id:1, customer:"nisha",amount:500, status:"completed"},
+    {id:2, customer:"virat",amount:1500, status:"inprogress"},
+    {id:3, customer:"anushka",amount:2500, status:"rejected"}
   ];
   res.send(orders);
 };
